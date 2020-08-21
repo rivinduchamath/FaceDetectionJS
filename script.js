e34a13875a400391dcc291c1915aa0dbb6da3b57
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-21 16:31:33 +0530

    Remove Mr

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ async function start() {
 }
 
 function loadLabeledImages() {
-    const labels = ['Rivindu Chamath' ,'Black Widow', 'Captain America', 'Captain Marvel']
+    const labels = ['Black Widow' , 'Captain America', 'Captain Marvel']
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
