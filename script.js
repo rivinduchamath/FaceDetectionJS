288b28554ad455bb1b97671a08ff2b2505851231
Rivindu Wijayarathna <trivinduchamath333@gmail.com>
2020-08-21 16:20:51 +0530

    Remove Mr

--- script.js
+++ script.js
@@ -36,12 +36,12 @@ async function start() {
 }
 
 function loadLabeledImages() {
-    const labels = ['Rivindu Chamath','Chamath']
+    const labels = ['Rivindu Chamath' ,'Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
     return Promise.all(
         labels.map(async label => {
             const descriptions = []
             for (let i = 1; i <= 2; i++) {
-                const img = await faceapi.fetchImage(`https://github.com/rivinduchamath/FaceDetectionJS/tree/master/labeled_images/${label}/${i}.jpg`)
+                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
                 const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
                 descriptions.push(detections.descriptor)
             }
